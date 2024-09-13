bb5bed5bc1c4105712534e3c4ebbe14f6f75e9d0
ChocoFlan-117 <bubal616@evit.edu>
2024-09-13 13:20:36 -0700

    Delete Python 3/Python 3 - Code/Reading Parts of a File - 2 directory

--- Python 3/Python 3 - Code/Reading Parts of a File - 2/Challenge - 1.py
+++
@@ -1,8 +0,0 @@
-#Read a Specific Number of Characters#
-
-integer = int(input("Choose a number: "))
-
-file = open("cobra.txt", "r")
-print(file.read(integer))
-
-file.close()
--- Python 3/Python 3 - Code/Reading Parts of a File - 2/Code Checkpoint.py
+++
@@ -1,8 +0,0 @@
-file = open(".txt", "r")
-
-print(file.read(10))
-
-print(file.readline())
-print(file.readline())
-
-file.close()
